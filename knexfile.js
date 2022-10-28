62ee947c0585fa945d1ccf4c0eb5642edd3a2cfa
datboiluiskrrt <datboiluiskrrt@gmail.com>
2022-10-27 22:04:32 -0500

    Added knex configuration to the database

--- knexfile.js
+++ knexfile.js
@@ -1,28 +1,15 @@
-const sharedConfig = {
-  client: "sqlite3",
-  useNullAsDefault: true,
-  migrations: {
-    directory: "./data/migrations",
-  },
-  seeds: {
-    directory: "./data/seeds",
-  },
-  pool: {
-    afterCreate: (conn, done) => {
-      conn.run("PRAGMA foreign_keys = ON", done);
-    },
-  },
-};
-
 module.exports = {
   development: {
-    ...sharedConfig,
+    client: "sqlite3",
     connection: {
-      // filename: '.'
+      filename: "./data/locations.db",
     },
-    testing: {
-      ...sharedConfig,
-      //   connection: { filename: "./data/" },
+    userNullAsDefault: true,
+    migrations: {
+      directory: "./data/migrations",
+    },
+    seeds: {
+      directory: "./data/seeds",
     },
   },
 };
