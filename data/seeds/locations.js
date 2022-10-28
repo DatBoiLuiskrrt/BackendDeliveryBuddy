eed375b29f34f5f04dbbcec3766626840e5569fe
datboiluiskrrt <datboiluiskrrt@gmail.com>
2022-10-27 22:54:54 -0500

    WIP

--- data/seeds/locations.js
+++ data/seeds/locations.js
@@ -1,13 +1,28 @@
 /**
  * @param { import("knex").Knex } knex
- * @returns { Promise<void> } 
+ * @returns { Promise<void> }
  */
-exports.seed = async function(knex) {
+//dependency injection
+exports.seed = async function (knex) {
   // Deletes ALL existing entries
-  await knex('table_name').del()
-  await knex('table_name').insert([
-    {id: 1, colName: 'rowValue1'},
-    {id: 2, colName: 'rowValue2'},
-    {id: 3, colName: 'rowValue3'}
+  //reset primary keys del() does not cut it
+  await knex("locations").truncate();
+  await knex("locations").insert([
+    //DELETE THE IDS!!!
+    {
+      comments: "He is not friendly",
+      address: "1035 Airport Lane, San Antonio, TX",
+      dangerous: true,
+    },
+    {
+      comments: "Sex offender",
+      address: "3049 Arroyo Lane, San Antonio, TX",
+      dangerous: true,
+    },
+    {
+      comments: "Has very dangerous dogs that attack you ",
+      address: "6942 Puerto Lane, San Antonio, TX",
+      dangerous: true,
+    },
   ]);
 };
