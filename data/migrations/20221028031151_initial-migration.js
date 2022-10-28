bf7e083ff0cb22a3b6064ff0deb265d5fc1e545c
datboiluiskrrt <datboiluiskrrt@gmail.com>
2022-10-27 22:30:55 -0500

    wip

--- data/migrations/20221028031151_initial-migration.js
+++ data/migrations/20221028031151_initial-migration.js
@@ -2,14 +2,23 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.up = function(knex) {
-  
+exports.up = function (knex) {
+  return knex.schema.createTable("locations", (tbl) => {
+    //creates an id column, auto inc integers
+    tbl.increments("locations_id");
+    //chainnig constraints
+    //notnullable = required
+    tbl.float("comments", 256).notNullable();
+    tbl.text("address", 256).notNullable();
+    //if not provided set to false by default
+    tbl.boolean("dangerous").defaultTo(false);
+  });
 };
 
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.down = function(knex) {
-  
+exports.down = function (knex) {
+  return knex.schema.dropTableIfExists("locations");
 };
